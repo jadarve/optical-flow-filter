0a01d96d06f01833c72702ee81d34882cafe2224
jadarve <juan.adarve@anu.edu.au>
2015-12-29 15:30:58 +1100

    Added clear() and copyFrom() methods to GPUImage.
    Also fixed the indentation style of the header and .cu files.

--- include/flowfilter/gpu/image.h
+++ include/flowfilter/gpu/image.h
@@ -16,131 +16,145 @@
 #include "flowfilter/image.h"
 
 namespace flowfilter {
-    namespace gpu {
+namespace gpu {
 
-        /**
-         * \brief struct to encapsulated image information.
-         *
-         * This structure is used internally in the kernel and
-         * device functions.
-         */
-        template<typename T>
-        struct gpuimage_t {
+/**
+ * \brief struct to encapsulated image information.
+ *
+ * This structure is used internally in the kernel and
+ * device functions.
+ */
+template<typename T>
+struct gpuimage_t {
 
-            /** height in pixels */
-            int height;
+    /** height in pixels */
+    int height;
 
-            /** width in pixels */
-            int width;
+    /** width in pixels */
+    int width;
 
-            /** row pitch in bytes*/
-            std::size_t pitch;
+    /** row pitch in bytes*/
+    std::size_t pitch;
 
-            /** memory buffer*/
-            T* data;
-        };
-
-
-        /*! \brief GPU Image container.
-         */
-        class GPUImage {
-
-        public:
-            GPUImage();
-            GPUImage(const int height, const int width,
-                const int depth = 1, const int itemSize = sizeof(char));
-
-            ~GPUImage();
-
-        public:
-            int height() const;
-            int width() const;
-            int depth() const;
-            int pitch() const;
-            int itemSize() const;
-
-            void* data();
-
-            template<typename T>
-            inline gpuimage_t<T> wrap() {
-                gpuimage_t<T> img;
-                img.height = __height;
-                img.width = __width;
-                img.pitch = __pitch;
-                img.data = (T*)__ptr_dev.get();
-                return img;
-            }
+    /** memory buffer*/
+    T* data;
+};
 
 
-            // TODO:: add stream parameter to support asynchrous copy
-            /**
-             * \brief upload an image in CPU to GPU memory space
-             */
-            void upload(flowfilter::image_t& img);
+/*! \brief GPU Image container.
+ */
+class GPUImage {
 
-            /**
-             * \brief download an image from GPU to CPU memory space
-             */
-            void download(flowfilter::image_t& img) const;
+public:
+    GPUImage();
+    GPUImage(const int height, const int width,
+             const int depth = 1, const int itemSize = sizeof(char));
 
+    ~GPUImage();
 
-        private:
-            std::size_t __width;
-            std::size_t __height;
-            std::size_t __depth;        // number of channels
-            std::size_t __pitch;        // row pitch in bytes
-            std::size_t __itemSize;     // item size in bytes
-            std::shared_ptr<void> __ptr_dev;
+public:
+    int height() const;
+    int width() const;
+    int depth() const;
+    int pitch() const;
+    int itemSize() const;
 
-        private:
-            void allocate();
-            bool compareShape(const flowfilter::image_t& img) const;
+    void* data();
 
-        };
-
-
-        class GPUTexture {
-
-        public:
-            GPUTexture();
-            GPUTexture( flowfilter::gpu::GPUImage& img, cudaChannelFormatKind format);
-            GPUTexture( flowfilter::gpu::GPUImage& img,
-                        cudaChannelFormatKind format,
-                        cudaTextureReadMode readMode);
-            GPUTexture( flowfilter::gpu::GPUImage& img,
-                        cudaChannelFormatKind format,
-                        cudaTextureAddressMode addressMode,
-                        cudaTextureFilterMode filterMode,
-                        cudaTextureReadMode readMode);
-
-            ~GPUTexture();
-
-        public:
-            cudaTextureObject_t getTextureObject();
-            flowfilter::gpu::GPUImage getImage();
+    template<typename T>
+    inline gpuimage_t<T> wrap() {
+        gpuimage_t<T> img;
+        img.height = __height;
+        img.width = __width;
+        img.pitch = __pitch;
+        img.data = (T*)__ptr_dev.get();
+        return img;
+    }
 
 
-        private:
-            void configure( cudaChannelFormatKind format,
-                            cudaTextureAddressMode addressMode,
-                            cudaTextureFilterMode filterMode,
-                            cudaTextureReadMode readMode);
+    // TODO:: add stream parameter to support asynchrous copy
+    /**
+     * \brief upload an image in CPU to GPU memory space
+     */
+    void upload(flowfilter::image_t& img);
 
-        private:
-            /** image buffer in GPU memory space */
-            flowfilter::gpu::GPUImage __image;
+    /**
+     * \brief download an image from GPU to CPU memory space
+     */
+    void download(flowfilter::image_t& img) const;
 
-            /** CUDA texture object */
-            cudaTextureObject_t __texture;
+    /**
+     * \brief copy the content of an image to this object.
+     */
+    void copyFrom(flowfilter::gpu::GPUImage& img);
 
-            // tells if the created texture is valid
-            bool __validTexture;
+    /**
+     * \brief set to zeros the image content
+     */
+    void clear();
 
-            /** reference counter for this object */
-            std::shared_ptr<int> __refCounter;
-        };
 
-    }; // namespace gpu
+private:
+    std::size_t __width;
+    std::size_t __height;
+    std::size_t __depth;        // number of channels
+    std::size_t __pitch;        // row pitch in bytes
+    std::size_t __itemSize;     // item size in bytes
+    std::shared_ptr<void> __ptr_dev;
+
+private:
+    void allocate();
+    bool compareShape(const flowfilter::image_t& img) const;
+    bool compareShapeGPU(const flowfilter::gpu::GPUImage& img) const;
+
+};
+
+
+class GPUTexture {
+
+public:
+    GPUTexture();
+
+    GPUTexture( flowfilter::gpu::GPUImage& img, cudaChannelFormatKind format);
+
+    GPUTexture( flowfilter::gpu::GPUImage& img,
+                cudaChannelFormatKind format,
+                cudaTextureReadMode readMode);
+
+    GPUTexture( flowfilter::gpu::GPUImage& img,
+                cudaChannelFormatKind format,
+                cudaTextureAddressMode addressMode,
+                cudaTextureFilterMode filterMode,
+                cudaTextureReadMode readMode);
+
+    ~GPUTexture();
+
+public:
+    cudaTextureObject_t getTextureObject();
+    flowfilter::gpu::GPUImage getImage();
+
+
+private:
+    void configure( cudaChannelFormatKind format,
+                    cudaTextureAddressMode addressMode,
+                    cudaTextureFilterMode filterMode,
+                    cudaTextureReadMode readMode);
+
+private:
+    /** image buffer in GPU memory space */
+    flowfilter::gpu::GPUImage __image;
+
+    /** CUDA texture object */
+    cudaTextureObject_t __texture;
+
+    // tells if the created texture is valid
+    bool __validTexture;
+
+    /** reference counter for this object */
+    std::shared_ptr<int> __refCounter;
+};
+
+}; // namespace gpu
 }; // namespace flowfilter
 
 
--- src/gpu/image.cu
+++ src/gpu/image.cu
@@ -7,290 +7,319 @@
 
 #include <cstring>
 #include <iostream>
+#include <exception>
 
 #include "flowfilter/gpu/image.h"
 #include "flowfilter/gpu/gpu_deleter.h"
 #include "flowfilter/gpu/error.h"
 
 namespace flowfilter {
-    namespace gpu {
+namespace gpu {
 
-        //#################################################
-        // GPUImage
-        //#################################################
-        GPUImage::GPUImage() {
-            __width = 0;
-            __height = 0;
-            __depth = 0;
-            __pitch = 0;
-            __itemSize = 0;
+//#################################################
+// GPUImage
+//#################################################
+GPUImage::GPUImage() {
+    __width = 0;
+    __height = 0;
+    __depth = 0;
+    __pitch = 0;
+    __itemSize = 0;
+}
+
+GPUImage::GPUImage(const int height, const int width,
+    const int depth, const int itemSize) {
+
+    __height = height;
+    __width = width;
+    __depth = depth;
+    __itemSize = itemSize;
+
+    // allocate memory in GPU space
+    allocate();
+}
+
+GPUImage::~GPUImage() {
+
+    // nothing to do
+    // device buffer is released by gpu_deleter
+    // std::cout << "GPUImage::~GPUImage(): [" << 
+    //     __height << ", " << __width << ", " << __depth << "] : " << __ptr_dev.use_count() << std::endl;
+}
+
+int GPUImage::height() const {
+    return __height;
+}
+
+int GPUImage::width() const {
+    return __width;
+}
+
+int GPUImage::depth() const {
+    return __depth;
+}
+
+int GPUImage::pitch() const {
+    return __pitch;
+}
+
+int GPUImage::itemSize() const {
+    return __itemSize;
+}
+
+void* GPUImage::data() {
+    return __ptr_dev.get();
+}
+
+
+void GPUImage::upload(flowfilter::image_t& img) {
+
+    // check if device memory is allocated
+    if(!__ptr_dev) {
+
+        // set resolution to input image
+        __width = img.width;
+        __height = img.height;
+        __depth = img.depth;
+        __itemSize = img.itemSize;
+
+        // allocate memory
+        allocate();
+    }
+
+    // compare shapes
+    if(compareShape(img)) {
+
+        // print first 5 elements of img
+        // for(int i = 0; i < 5; i ++) {
+        //     std::cout << i << ": " << (int)static_cast<unsigned char*>(img.data)[i] << std::endl;
+        // }
+
+        // issue synchronous memory copy
+        checkError(cudaMemcpy2D(__ptr_dev.get(), __pitch, img.data, img.pitch,
+            __width*__depth*__itemSize, __height,
+            cudaMemcpyHostToDevice));
+
+        // TODO: add support for asynchronous copy
+
+    } else {
+        std::cerr << "ERROR: GPUImage::upload(): shapes do not match" << std::endl;
+        return; // TODO: throw exception
+    }
+}
+
+void GPUImage::download(flowfilter::image_t& img) const {
+
+    if(!__ptr_dev) {
+        std::cerr << "ERROR: GPUImage::download(): unallocated image" << std::endl;
+        return; // TODO: throw exception
+    }
+
+    if(compareShape(img)) {
+
+        // issue synchronous memory copy
+        checkError(cudaMemcpy2D(img.data, img.pitch, __ptr_dev.get(), __pitch,
+            __width*__depth*__itemSize, __height, cudaMemcpyDeviceToHost));
+
+        // print first 5 elements of img
+        // for(int i = 0; i < 5; i ++) {
+        //     std::cout << i << ": " << static_cast<float*>(img.data)[i] << std::endl;
+        // }
+
+    } else {
+        std::cerr << "ERROR: GPUImage::download(): shapes do not match" << std::endl;
+        return; // TODO: throw exception
+    }
+}
+
+void GPUImage::copyFrom(GPUImage& img) {
+
+    if(compareShapeGPU(img)) {
+
+        // issue synchronous memory copy
+        checkError(cudaMemcpy2D(__ptr_dev.get(), __pitch, 
+            img.__ptr_dev.get(), img.__pitch,
+            __width*__depth*__itemSize, __height, cudaMemcpyDeviceToDevice));
+
+    } else {
+        std::cerr << "ERROR: GPUImage::copyFrom(): image shapes do not match" << std::endl;
+        throw std::exception();
+    }
+}
+
+void GPUImage::clear() {
+
+    checkError(cudaMemset2D(__ptr_dev.get(), __pitch,
+        0, __width*__depth*__itemSize, __height));
+}
+
+
+void GPUImage::allocate() {
+
+    // std::cout << "GPUImage::allocate()" << std::endl;
+
+    void* buffer_dev = nullptr;
+    checkError(cudaMallocPitch(&buffer_dev, &__pitch,
+        __width*__depth*__itemSize, __height));
+
+    // create a new shared pointer
+    __ptr_dev = std::shared_ptr<void> {buffer_dev, gpu_deleter<void>()};
+
+    // std::cout << "\tpitch: " << __pitch << std::endl;
+
+    // if(err != cudaSuccess) {
+    //     std::cerr << "ERROR: GPUImage device memory allocation: "
+    //         << cudaGetErrorString(err) << std::endl;
+    //     // TODO: throw exception?
+    // }
+}
+
+bool GPUImage::compareShape(const flowfilter::image_t& img) const {
+
+    return __height == img.height &&
+        __width == img.width &&
+        __depth == img.depth &&
+        __itemSize == img.itemSize;
+}
+
+bool GPUImage::compareShapeGPU(const flowfilter::gpu::GPUImage& img) const {
+
+    return __height == img.height() &&
+        __width == img.width() &&
+        __depth == img.depth() &&
+        __pitch == img.pitch() &&
+        __itemSize == img.itemSize();
+}
+
+
+
+//#################################################
+// GPUTexture
+//#################################################
+GPUTexture::GPUTexture() {
+
+    // texture object is not valid
+    __validTexture = false;
+    __refCounter = std::make_shared<int>(0);
+}
+
+GPUTexture::GPUTexture( GPUImage& img, cudaChannelFormatKind format) :
+    GPUTexture(img, format, cudaAddressModeClamp, cudaFilterModePoint, cudaReadModeElementType) {
+}
+
+GPUTexture::GPUTexture( GPUImage& img,
+                        cudaChannelFormatKind format,
+                        cudaTextureReadMode readMode) : 
+    GPUTexture(img, format, cudaAddressModeClamp, cudaFilterModePoint, readMode) {
+
+}
+
+GPUTexture::GPUTexture( GPUImage& img,
+                        cudaChannelFormatKind format,
+                        cudaTextureAddressMode addressMode,
+                        cudaTextureFilterMode filterMode,
+                        cudaTextureReadMode readMode) {
+
+    __refCounter = std::make_shared<int>(0);
+
+    // hold input image
+    __image = img;
+
+    // configure CUDA texture
+    configure(format, addressMode, filterMode, readMode);
+}
+
+GPUTexture::~GPUTexture() {
+
+    // std::cout << "GPUTexture::~GPUTexture(): " <<  __refCounter.use_count() << " : " << __texture << std::endl;
+
+
+    // only attempts to destroy the texture if the creation
+    // was successful
+    if(__refCounter.use_count() == 1) {
+        if(__validTexture) {
+            // std::cout << "\tdestroying texture" << std::endl;
+            checkError(cudaDestroyTextureObject(__texture));    
         }
-
-        GPUImage::GPUImage(const int height, const int width,
-            const int depth, const int itemSize) {
-
-            __height = height;
-            __width = width;
-            __depth = depth;
-            __itemSize = itemSize;
-
-            // allocate memory in GPU space
-            allocate();
-        }
-
-        GPUImage::~GPUImage() {
-
-            // nothing to do
-            // device buffer is released by gpu_deleter
-            // std::cout << "GPUImage::~GPUImage(): [" << 
-            //     __height << ", " << __width << ", " << __depth << "] : " << __ptr_dev.use_count() << std::endl;
-        }
-
-        int GPUImage::height() const {
-            return __height;
-        }
-
-        int GPUImage::width() const {
-            return __width;
-        }
-
-        int GPUImage::depth() const {
-            return __depth;
-        }
-
-        int GPUImage::pitch() const {
-            return __pitch;
-        }
-
-        int GPUImage::itemSize() const {
-            return __itemSize;
-        }
-
-        void* GPUImage::data() {
-            return __ptr_dev.get();
-        }
-
-
-        void GPUImage::upload(flowfilter::image_t& img) {
-
-            // check if device memory is allocated
-            if(!__ptr_dev) {
-
-                // set resolution to input image
-                __width = img.width;
-                __height = img.height;
-                __depth = img.depth;
-                __itemSize = img.itemSize;
-
-                // allocate memory
-                allocate();
-            }
-
-            // compare shapes
-            if(compareShape(img)) {
-
-                // print first 5 elements of img
-                // for(int i = 0; i < 5; i ++) {
-                //     std::cout << i << ": " << (int)static_cast<unsigned char*>(img.data)[i] << std::endl;
-                // }
-
-                // issue synchronous memory copy
-                checkError(cudaMemcpy2D(__ptr_dev.get(), __pitch, img.data, img.pitch,
-                    __width*__depth*__itemSize, __height,
-                    cudaMemcpyHostToDevice));
-
-                // TODO: add support for asynchronous copy
-
-            } else {
-                std::cerr << "ERROR: GPUImage::upload(): shapes do not match" << std::endl;
-                return; // TODO: throw exception
-            }
-        }
-
-        void GPUImage::download(flowfilter::image_t& img) const {
-
-            if(!__ptr_dev) {
-                std::cerr << "ERROR: GPUImage::download(): unallocated image" << std::endl;
-                return; // TODO: throw exception
-            }
-
-            if(compareShape(img)) {
-
-                // issue synchronous memory copy
-                checkError(cudaMemcpy2D(img.data, img.pitch, __ptr_dev.get(), __pitch,
-                    __width*__depth*__itemSize, __height, cudaMemcpyDeviceToHost));
-
-                // print first 5 elements of img
-                // for(int i = 0; i < 5; i ++) {
-                //     std::cout << i << ": " << static_cast<float*>(img.data)[i] << std::endl;
-                // }
-
-            } else {
-                std::cerr << "ERROR: GPUImage::download(): shapes do not match" << std::endl;
-                return; // TODO: throw exception
-            }
-        }
-
-
-        void GPUImage::allocate() {
-
-            // std::cout << "GPUImage::allocate()" << std::endl;
-
-            void* buffer_dev = nullptr;
-            checkError(cudaMallocPitch(&buffer_dev, &__pitch,
-                __width*__depth*__itemSize, __height));
-
-            // create a new shared pointer
-            __ptr_dev = std::shared_ptr<void> {buffer_dev, gpu_deleter<void>()};
-
-            // std::cout << "\tpitch: " << __pitch << std::endl;
-
-            // if(err != cudaSuccess) {
-            //     std::cerr << "ERROR: GPUImage device memory allocation: "
-            //         << cudaGetErrorString(err) << std::endl;
-            //     // TODO: throw exception?
-            // }
-        }
-
-        bool GPUImage::compareShape(const flowfilter::image_t& img) const {
-
-            return __height == img.height &&
-                __width == img.width &&
-                __depth == img.depth &&
-                __itemSize == img.itemSize;
-        }
-
-
-
-        //#################################################
-        // GPUTexture
-        //#################################################
-        GPUTexture::GPUTexture() {
-
-            // texture object is not valid
-            __validTexture = false;
-            // __refCounter = std::shared_ptr<int>(new int(0));
-            __refCounter = std::make_shared<int>(0);
-        }
-
-        GPUTexture::GPUTexture( GPUImage& img, cudaChannelFormatKind format) :
-            GPUTexture(img, format, cudaAddressModeClamp, cudaFilterModePoint, cudaReadModeElementType) {
-        }
-
-        GPUTexture::GPUTexture( GPUImage& img,
-                                cudaChannelFormatKind format,
-                                cudaTextureReadMode readMode) : 
-            GPUTexture(img, format, cudaAddressModeClamp, cudaFilterModePoint, readMode) {
-
-        }
-
-        GPUTexture::GPUTexture( GPUImage& img,
-                                cudaChannelFormatKind format,
-                                cudaTextureAddressMode addressMode,
-                                cudaTextureFilterMode filterMode,
-                                cudaTextureReadMode readMode) {
-
-            // __refCounter = std::shared_ptr<int>(new int(0));
-            __refCounter = std::make_shared<int>(0);
-
-            // hold input image
-            __image = img;
-
-            // configure CUDA texture
-            configure(format, addressMode, filterMode, readMode);
-        }
-
-        GPUTexture::~GPUTexture() {
-
-            // std::cout << "GPUTexture::~GPUTexture(): " <<  __refCounter.use_count() << " : " << __texture << std::endl;
-
-
-            // only attempts to destroy the texture if the creation
-            // was successful
-            if(__refCounter.use_count() == 1) {
-                if(__validTexture) {
-                    // std::cout << "\tdestroying texture" << std::endl;
-                    checkError(cudaDestroyTextureObject(__texture));    
-                }
-            }
-
-            // __image destructor is called automatically and
-            // devide buffer is released only if it's not being
-            // shared in any other part of the program.
-        }
-
-        cudaTextureObject_t GPUTexture::getTextureObject() {
-            return __texture;
-        }
-
-        GPUImage GPUTexture::getImage() {
-            return __image;
-        }
-
-        void GPUTexture::configure( cudaChannelFormatKind format,
-                                    cudaTextureAddressMode addressMode,
-                                    cudaTextureFilterMode filterMode,
-                                    cudaTextureReadMode readMode) {
-            
-            __validTexture = false;
-
-            int channels = __image.depth();
-            if(channels > 4) {
-                std::cerr << "ERROR: GPUTexture::configure(): image channels greater than 4: " << channels << std::endl;
-                return;
-            }
-
-            // bit width of element
-            int bitWidth = 8 * __image.itemSize();
-
-            // bit width of each channel
-            int w1 = bitWidth;  // there is at least one channel
-            int w2 = channels >= 2? bitWidth : 0;
-            int w3 = channels >= 3? bitWidth : 0;
-            int w4 = channels == 4? bitWidth : 0;
-
-            // std::cout << "GPUTexture::configure(): channel width: [" << w1 << ", " << w2 << ", " << w3 << ", " << w4 << "]" << std::endl;
-            // std::cout << "GPUTexture::configure(): [" << __image.height() << ", " << __image.width() << ", " << __image.depth() << "] size: " << __image.itemSize() << " pitch: " << __image.pitch() << std::endl;
-
-            // channel descriptor
-            cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(w1, w2, w3, w4, format);
-
-            // texture descriptor
-            cudaTextureDesc texDesc;
-            memset(&texDesc, 0, sizeof(texDesc));
-            texDesc.addressMode[0] = addressMode;
-            texDesc.addressMode[1] = addressMode;
-            texDesc.filterMode = filterMode;
-            texDesc.readMode = readMode;
-            texDesc.normalizedCoords = false;
-
-            // texture buffer descriptor
-            cudaResourceDesc resDesc;
-            memset(&resDesc, 0, sizeof(resDesc));
-            resDesc.resType = cudaResourceTypePitch2D;
-            resDesc.res.pitch2D.desc = channelDesc;
-            resDesc.res.pitch2D.devPtr = __image.data();
-            resDesc.res.pitch2D.pitchInBytes = __image.pitch();
-            resDesc.res.pitch2D.width = __image.width();
-            resDesc.res.pitch2D.height = __image.height();
-
-            // creates texture
-            checkError(cudaCreateTextureObject(&__texture, &resDesc, &texDesc, NULL));
-            __validTexture = true;
-
-            // cudaError_t err = cudaCreateTextureObject(&__texture, &resDesc, &texDesc, NULL);
-
-            // std::cout << "GPUTexture::configure(): texture ID: " << __texture << std::endl;
-            // if(err == cudaSuccess) {
-            //     __validTexture = true;
-            // } else {
-
-            //     std::cerr << "ERROR: GPUTexture::configure(): texture creation: "
-            //         << cudaGetErrorString(err) << std::endl;
-
-            //     __validTexture = false;
-            // }
-        }
-
-    }; // namespace gpu
+    }
+
+    // __image destructor is called automatically and
+    // devide buffer is released only if it's not being
+    // shared in any other part of the program.
+}
+
+cudaTextureObject_t GPUTexture::getTextureObject() {
+    return __texture;
+}
+
+GPUImage GPUTexture::getImage() {
+    return __image;
+}
+
+void GPUTexture::configure( cudaChannelFormatKind format,
+                            cudaTextureAddressMode addressMode,
+                            cudaTextureFilterMode filterMode,
+                            cudaTextureReadMode readMode) {
+    
+    __validTexture = false;
+
+    int channels = __image.depth();
+    if(channels > 4) {
+        std::cerr << "ERROR: GPUTexture::configure(): image channels greater than 4: " << channels << std::endl;
+        return;
+    }
+
+    // bit width of element
+    int bitWidth = 8 * __image.itemSize();
+
+    // bit width of each channel
+    int w1 = bitWidth;  // there is at least one channel
+    int w2 = channels >= 2? bitWidth : 0;
+    int w3 = channels >= 3? bitWidth : 0;
+    int w4 = channels == 4? bitWidth : 0;
+
+    // std::cout << "GPUTexture::configure(): channel width: [" << w1 << ", " << w2 << ", " << w3 << ", " << w4 << "]" << std::endl;
+    // std::cout << "GPUTexture::configure(): [" << __image.height() << ", " << __image.width() << ", " << __image.depth() << "] size: " << __image.itemSize() << " pitch: " << __image.pitch() << std::endl;
+
+    // channel descriptor
+    cudaChannelFormatDesc channelDesc = cudaCreateChannelDesc(w1, w2, w3, w4, format);
+
+    // texture descriptor
+    cudaTextureDesc texDesc;
+    memset(&texDesc, 0, sizeof(texDesc));
+    texDesc.addressMode[0] = addressMode;
+    texDesc.addressMode[1] = addressMode;
+    texDesc.filterMode = filterMode;
+    texDesc.readMode = readMode;
+    texDesc.normalizedCoords = false;
+
+    // texture buffer descriptor
+    cudaResourceDesc resDesc;
+    memset(&resDesc, 0, sizeof(resDesc));
+    resDesc.resType = cudaResourceTypePitch2D;
+    resDesc.res.pitch2D.desc = channelDesc;
+    resDesc.res.pitch2D.devPtr = __image.data();
+    resDesc.res.pitch2D.pitchInBytes = __image.pitch();
+    resDesc.res.pitch2D.width = __image.width();
+    resDesc.res.pitch2D.height = __image.height();
+
+    // creates texture
+    checkError(cudaCreateTextureObject(&__texture, &resDesc, &texDesc, NULL));
+    __validTexture = true;
+
+    // cudaError_t err = cudaCreateTextureObject(&__texture, &resDesc, &texDesc, NULL);
+
+    // std::cout << "GPUTexture::configure(): texture ID: " << __texture << std::endl;
+    // if(err == cudaSuccess) {
+    //     __validTexture = true;
+    // } else {
+
+    //     std::cerr << "ERROR: GPUTexture::configure(): texture creation: "
+    //         << cudaGetErrorString(err) << std::endl;
+
+    //     __validTexture = false;
+    // }
+}
+
+}; // namespace gpu
 }; // namespace flowfilter
