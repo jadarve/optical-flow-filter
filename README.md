8094ee72da7ccfa4d486b106d1db45399ebdb1bf
jadarve <juan.adarve@anu.edu.au>
2016-01-08 17:26:19 +1100

    Added demos folder with webcam sample application.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# optical-flow-filter
-A real time optical flow algorithm implemented on GPU
+# Optical-flow-filter
+
+A real time optical flow algorithm implemented on GPU.
+
+
+# Build and Installation
+
+    git clone https://github.com/jadarve/optical-flow-filter.git
+    cd optical-flow-filter
+    mkdir build
+    cd build
+    cmake ..
+    make
+    sudo make install 
+    
+The library and header files will be installed at **/usr/local/lib** and **/usr/local/include** respectively.
+
+##Python Wrappers
+
+A python package with wrappers to the C++ library is available at **optical-flow-filter/python/** folder. The wrappers have been developed and build using Cython 0.23.4.
+
+    cd optical-flow-filter/python/
+    python setup.py build
+    sudo python setup.py install
+
+See **notebooks/** folder for usage examples.
+
+# Demo Applications
+
+Demo applications are available under the **demos** folder. To build  **flowWebCam** application to compute optical flow from webCam video follow the instructions bellow. These application requires OpenCV to be installed in the system.
+
+    cd optical-flow-filter/demos/flowWebCam
+    mkdir build
+    cd build
+    cmake ..
+    make
+    ./flowWebCam
+    
+
+# References
+
+ Article under review on Robotics and Automation Letters (RA-L)
+
---
+++ demos/flowWebCam/src/flowWebCam.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <vector>
+
+#include <opencv2/opencv.hpp>
+
+#include <flowfilter/gpu/flowfilter.h>
+#include <flowfilter/gpu/display.h>
+
+using namespace std;
+using namespace cv;
+using namespace flowfilter;
+using namespace flowfilter::gpu;
+
+
+void wrapCVMat(Mat& cvMat, image_t& img) {
+
+    img.height = cvMat.rows;
+    img.width = cvMat.cols;
+    img.depth = cvMat.channels();
+    img.pitch = cvMat.cols*cvMat.elemSize();
+    img.itemSize = cvMat.elemSize1();
+    img.data = cvMat.ptr();
+}
+
+
+int main(int argc, char** argv) {
+
+    VideoCapture cap(0); // open the default camera
+    if(!cap.isOpened()){
+        return -1;
+    }
+    
+    Mat frame;
+    
+
+    //  captura a frame to get image width and height
+    cap >> frame;
+    int width = frame.cols;
+    int height = frame.rows;
+    cout << "frame shape: [" << height << ", " << width << "]" << endl;
+
+    Mat frameGray(height, width, CV_8UC1);
+    Mat fcolor(height, width, CV_8UC4);
+
+    // structs used to wrap cv::Mat images and transfer to flowfilter
+    image_t hostImageGray;
+    image_t hostFlowColor;
+
+    wrapCVMat(frameGray, hostImageGray);
+    wrapCVMat(fcolor, hostFlowColor);
+    
+
+    //#################################
+    // Filter parameters
+    //#################################
+    float maxflow = 20.0f;
+    vector<float> gamma = {500.0f, 50.0f, 5.0f};
+    vector<int> smoothIterations = {2, 8, 20};
+
+    //#################################
+    // Filter creation with
+    // 3 pyramid levels
+    //#################################
+    PyramidalFlowFilter filter(height, width, 3);
+    filter.setMaxFlow(maxflow);
+    filter.setGamma(gamma);
+    filter.setSmoothIterations(smoothIterations);
+    
+
+    // Color encoder connected to optical flow buffer in the GPU
+    FlowToColor flowColor(filter.getFlow(), maxflow);
+
+
+    // Capture loop
+    for(;;) {
+
+        // capture a new frame from the camera
+        // and convert it to gray scale (uint8)
+        cap >> frame;
+        cvtColor(frame, frameGray, CV_BGR2GRAY);
+        
+        // transfer image to flow filter and compute
+        filter.loadImage(hostImageGray);
+        filter.compute();
+
+        cout << "elapsed time: " << filter.elapsedTime() << " ms" << endl;
+
+        // computes color encoding (RGBA) and download it to host
+        flowColor.compute();
+        flowColor.downloadColorFlow(hostFlowColor);
+
+        imshow("image", frameGray);
+        imshow("optical flow", fcolor);
+
+        if(waitKey(1) >= 0) break;
+    }
+
+    // the camera will be deinitialized automatically in VideoCapture destructor
+    return 0;
+}
