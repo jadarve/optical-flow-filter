c2f0301530f6745bd202593d980253784c795e3c
jadarve <juan.adarve@anu.edu.au>
2016-04-06 20:18:06 +1000

    Started defining ImageRotationPredictor class and kernels.

--- include/flowfilter/gpu/device/propagation_k.h
+++ include/flowfilter/gpu/device/propagation_k.h
@@ -11,6 +11,8 @@
 
 #include "flowfilter/gpu/image.h"
 
+#include "flowfilter/gpu/device/image_k.h"
+
 
 namespace flowfilter {
 namespace gpu {
@@ -43,6 +45,90 @@ __global__ void flowPropagatePayloadY_k(cudaTextureObject_t inputFlow,
                                         const float dt, const int border);
 
 
+template<typename T>
+__global__ void LaxWendroffX_k(cudaTextureObject_t inputFlow,
+                               cudaTextureObject_t inputImage,
+                               gpuimage_t<T> propagatedImage,
+                               const float dt) {
+
+        const int height = propagatedImage.height;
+        const int width = propagatedImage.width;
+
+        // pixel coordinate
+        const int2 pix = make_int2(blockIdx.x * blockDim.x + threadIdx.x,
+                                   blockIdx.y * blockDim.y + threadIdx.y);
+
+        if (pix.x >= width || pix.y >= height) {
+                return;
+        }
+
+        const float2 flow = tex2D<float2>(inputFlow, pix.x, pix.y);
+
+        // image values
+        T img_m = tex2D<T>(inputImage, pix.x - 1, pix.y);
+        T img_0 = tex2D<T>(inputImage, pix.x, pix.y);
+        T img_p = tex2D<T>(inputImage, pix.x + 1, pix.y);
+
+
+        // central difference
+        T diff_0 = img_p - img_0;
+
+        // second difference
+        T diff2 = img_m - 2*img_0 + img_p;
+
+        float R = dt*flow.x;
+
+        // Lax-Wendroff scheme
+        T img_prop = img_0 - 0.5*R*diff_0 + 0.5*R*R*diff2;
+
+        //#################################
+        // PACK RESULTS
+        //#################################
+        *coordPitch(propagatedImage, pix) = img_prop;
+}
+
+
+template<typename T>
+__global__ void LaxWendroffY_k(cudaTextureObject_t inputFlow,
+                               cudaTextureObject_t inputImage,
+                               gpuimage_t<T> propagatedImage,
+                               const float dt) {
+
+        const int height = propagatedImage.height;
+        const int width = propagatedImage.width;
+
+        // pixel coordinate
+        const int2 pix = make_int2(blockIdx.x * blockDim.x + threadIdx.x,
+                                   blockIdx.y * blockDim.y + threadIdx.y);
+
+        if (pix.x >= width || pix.y >= height) {
+                return;
+        }
+
+        const float2 flow = tex2D<float2>(inputFlow, pix.x, pix.y);
+
+        // image values
+        T img_m = tex2D<T>(inputImage, pix.x, pix.y - 1);
+        T img_0 = tex2D<T>(inputImage, pix.x, pix.y);
+        T img_p = tex2D<T>(inputImage, pix.x, pix.y + 1);
+
+
+        // central difference
+        T diff_0 = img_p - img_0;
+
+        // second difference
+        T diff2 = img_m - 2*img_0 + img_p;
+
+        float R = dt*flow.y;
+
+        // Lax-Wendroff scheme
+        T img_prop = img_0 - 0.5*R*diff_0 + 0.5*R*R*diff2;
+
+        //#################################
+        // PACK RESULTS
+        //#################################
+        *coordPitch(propagatedImage, pix) = img_prop;
+}
 
 
 }; // namespace gpu
