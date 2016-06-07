d119de973b2fcc1fb633afb397543fa44d5f5df6
Juan David Adarve <juan.adarve@anu.edu.au>
2016-06-07 20:56:37 +1000

    Added license header to osconfig.h

--- include/flowfilter/osconfig.h
+++ include/flowfilter/osconfig.h
@@ -1,10 +1,12 @@
-
+/**
+ * \file osconfig.h
+ * \brief Operating System configuration file.
+ * \copyright 2015, Juan David Adarve, ANU. See AUTHORS for more details
+ * \license 3-clause BSD, see LICENSE for more details
+ */
 #ifndef FLOWFILTER_OSCONFIG_H_
 #define FLOWFILTER_OSCONFIG_H_
 
-
-// #define XSTR(x) STR(x)
-// #define STR(x) #x
 
 // if compilling in a Windows environment
 #if defined(_WIN32)
